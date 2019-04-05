107c3d4142ec6457ee72db5e30df7ef74dfa2890
Nikolay Kelov <n_kelov@yahoo.com>
2019-04-05 21:06:08 +0200

    Started extra exercise Problem 1

---
+++ 07.Associative Arrays/07.AssociativeArraysMoreEx/07.AssociativeArraysMoreEx/P01.Ranking/Ranking.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.Ranking
+{
+    class Ranking
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            var contestAndPass = new Dictionary<string, string>(); // key == contest, value == password
+
+            while ((input = Console.ReadLine()) != "end of contests")
+            {
+                string[] inputArray = input.Split(":").ToArray();
+                string contest = inputArray[0];
+                string pass = inputArray[1];
+
+                contestAndPass.Add(contest, pass);
+            }
+
+            var nameContestPoints = new Dictionary<string, Dictionary<string, int>>();
+            // ^ first Key is the Username, second Key is the contest, the Value is the Points
+
+
+            while ((input = Console.ReadLine()) != "end of submissions")
+            {
+                string[] inputArray = input.Split("=>").ToArray();
+                string contest = inputArray[0];
+                string pass = inputArray[1];
+                string name = inputArray[2];
+                int points = int.Parse(inputArray[3]);
+
+                if (contestAndPass.ContainsKey(contest))
+                {
+                    if (contestAndPass[contest] == pass)
+                    {
+                        if (nameContestPoints.ContainsKey(name))
+                        {
+                            if (nameContestPoints[name].ContainsKey(contest))
+                            {
+                                if (nameContestPoints[name][contest] < points)
+                                {
+                                    nameContestPoints[name][contest] = points;
+                                }
+                            }
+                            else
+                            {
+                                nameContestPoints[name].Add(contest, points);
+                            }
+                        }
+                        else
+                        {
+                            nameContestPoints.Add(name, new Dictionary<string, int>());
+                            nameContestPoints[name].Add(contest, points);
+                        }
+                    }
+                }
+
+            }
+
+            var nameTotalPoints = new Dictionary<string, int>(); // key == username, value == total points in all contests
+
+            foreach (var name in nameContestPoints)
+            {
+                int sum = 0;
+                string key = name.Key;
+                foreach (var contest in nameContestPoints[key])
+                {
+                    sum += contest.Value;
+                }
+                nameTotalPoints.Add(key, sum);
+            }
+
+            string winner = nameTotalPoints.FirstOrDefault(x => x.Value == nameTotalPoints.Values.Max()).Key;
+            string winnerName = 
+            Console.WriteLine($"Best candidate is {} with total {} points.");
+        }
+    }
+}
