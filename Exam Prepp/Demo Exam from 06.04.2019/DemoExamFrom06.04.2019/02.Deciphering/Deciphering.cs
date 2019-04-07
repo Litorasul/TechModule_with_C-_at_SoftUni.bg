b5aa8f39ff229bec9363b41e613a5b0a1ced62f6
Nikolay Kelov <n_kelov@yahoo.com>
2019-04-07 20:10:25 +0200

    Demo Exam from 06/04/2019 Problem 1 and Problem 2 both 100 out of 100 points

---
+++ Exam Prepp/Demo Exam from 06.04.2019/DemoExamFrom06.04.2019/02.Deciphering/Deciphering.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02.Deciphering
+{
+    class Deciphering
+    {
+        static void Main(string[] args)
+        {
+            string encryptedString = Console.ReadLine();
+            string pattern = @"^[d-z\{\}\|\#]*$";
+            Regex validText = new Regex(pattern);
+            Match isValid = validText.Match(encryptedString);
+            string[] toReplace = Console.ReadLine().Split().ToArray();
+            string old = toReplace[0];
+            string replacer = toReplace[1];
+            StringBuilder decryptedString = new StringBuilder();
+
+            if (isValid.Success)
+            {
+                foreach (char letter in encryptedString.ToCharArray())
+                { 
+                    char curentChar = (char)(letter - 3);
+                    decryptedString.Append(curentChar);
+                }
+
+                decryptedString.Replace(old, replacer);
+
+                Console.WriteLine(decryptedString);
+            }
+            else
+            {
+                Console.WriteLine("This is not the book you are looking for.");
+
+            }
+        }
+    }
+}
---
+++ Exam Prepp/Demo Exam from 06.04.2019/DemoExamFrom06.04.2019/Dictionary/Dictionarys.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dictionary
+{
+    class Dictionarys
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" | ").ToArray();
+
+            //the Key is the word the Value is a list with definitions.
+            var wordDefinition = new Dictionary<string, List<string>>();
+
+            foreach (var item in input)
+            {
+                string[] inputArray = item.Split(": ").ToArray();
+                string word = inputArray[0];
+                string definition = inputArray[1];
+
+                if (wordDefinition.ContainsKey(word))
+                {
+                    wordDefinition[word].Add(definition);
+                }
+                else
+                {
+                    wordDefinition.Add(word, new List<string>());
+                    wordDefinition[word].Add(definition);
+                }
+            }
+
+            input = Console.ReadLine().Split(" | ").ToArray();
+            foreach (var word in input)
+            {
+                if (wordDefinition.ContainsKey(word))
+                {
+                    Console.WriteLine($"{word}");
+                    foreach (var definition in wordDefinition[word].OrderByDescending(x => x.Length))
+                    {
+                        Console.WriteLine($" -{definition}");
+                    }
+                }
+            }
+            string finalInput = Console.ReadLine();
+            if (finalInput == "End")
+            {
+                return;
+            }
+            else
+            {
+                foreach (var word in wordDefinition.OrderBy(g => g.Key))
+                {
+                    Console.Write($"{word.Key} ");
+                }
+            }
+        }
+    }
+}
