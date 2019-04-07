1db788c40fa3a4c3790251d1d6f6e58a90ba6605
Nikolay Kelov <n_kelov@yahoo.com>
2019-04-07 21:55:28 +0200

    Started Web Project

---
+++ Exam Prepp/Demo Exam from 06.04.2019/Skeleton-C#/Skeleton-C#/Library/Controllers/LibraryController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Library.Data;
+using Library.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    public class LibraryController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            using (var db = new LibraryDbContext())
+            {
+                List<Book> allBooks = db.Books.ToList();
+                return this.View(allBooks);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            using (var db = new LibraryDbContext())
+            {
+                db.Books.Add(book);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            //TO DO
+            return null;
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Book book)
+        {
+            //TO DO
+            return null;
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            //TO DO
+            return null;
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Book book)
+        {
+            //TO DO
+            return null;
+        }
+    }
+}
